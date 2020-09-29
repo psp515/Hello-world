12c8f79dfff084e67c6f233e0ffdc85e4561f302
psp515 <kolberu@gmail.com>
2020-09-29 22:09:11 +0200

    Hello-World

---
+++ Hello-World-App (.NET Core)/Hello-World-App (.NET Core)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Hello_World_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("You are Welcome!");
+            Console.WriteLine();
+            WriteHello();
+            Console.WriteLine("Hello World x 5?");
+            WriteHello_n(5);
+        }
+
+        private static void WriteHello() => Console.WriteLine("Hello World!");
+        private static void WriteHello_n(int n) 
+        { 
+            for (int i=0;i<n ;i++ )
+                Console.WriteLine("{0}. Hello World!",n);
+        }
+    }
+}
